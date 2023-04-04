40092844ea48216975038a96d934eb6ef031094d
CoffeeJunioR <superrr.den@list.ru>
2023-04-04 19:56:11 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = b**2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+} else if (d == 0) {
+    arr.push(-b/(2*a));
+    return arr;
+} else if (d > 0) {
+    arr.push((-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a));
+    return arr;
+};
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / countMonths / 100;
+  let moneyForReturn = amount - contribution;
+  let payment = moneyForReturn * (percent + (percent / (((1 + percent)**countMonths) - 1)));
+  let fullPayment = Math.round(payment * countMonths * 100) / 100;
+  return fullPayment;
 }
