94c64bf1fd44a87d4c9180b8a21deb245dc89fb9
CoffeeJunioR <superrr.den@list.ru>
2023-04-06 11:51:50 +0300

    Fixed second exercise

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = b**2 - 4 * a * c;
-  if (d < 0) {
-    return arr;
-} else if (d == 0) {
-    arr.push(-b/(2*a));
-    return arr;
-} else if (d > 0) {
-    arr.push((-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a));
-    return arr;
+  const solve = [];
+  const discriminant = b**2 - 4 * a * c;
+  if (discriminant < 0) {
+    return solve;
+} else if (discriminant == 0) {
+  solve.push(-b/(2*a));
+    return solve;
+} else if (discriminant > 0) {
+  solve.push((-b + Math.sqrt(discriminant) )/(2*a), (-b - Math.sqrt(discriminant) )/(2*a));
+    return solve;
 };
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percent = percent / countMonths / 100;
+  percent = percent / 12 / 100;
   let moneyForReturn = amount - contribution;
   let payment = moneyForReturn * (percent + (percent / (((1 + percent)**countMonths) - 1)));
   let fullPayment = Math.round(payment * countMonths * 100) / 100;
