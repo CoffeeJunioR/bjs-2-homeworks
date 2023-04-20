25f2ed68082af5f92ad9eae3d87ebef2a85a5da8
CoffeeJunioR <superrr.den@list.ru>
2023-04-20 20:11:39 +0300

    Fixed objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,11 +2,11 @@ function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
   this.age = age;
+  this.marks = [];
 }
 let student1 = new Student("Ivan", "Male", 18);
 let student2 = new Student("Denis", "Male", 19);
 let student3 = new Student("Maria", "Female", 20);
-Student.marks = [];
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
@@ -16,7 +16,9 @@ student2.setSubject("History");
 student3.setSubject("Geography");
 
 Student.prototype.addMarks = function (...marks) {
-  (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
+  if (this.marks != undefined) {
+    this.marks.push(...marks);
+    }
 };
 student1.addMarks(3, 4, 5, 2);
 student2.addMarks(5, 5, 5);
@@ -25,15 +27,14 @@ student3.addMarks(2, 2, 2, 2);
 Student.prototype.getAverage = function () {
   if ((this.marks == undefined) || (this.marks.length == 0)) {
     return 0
-  } else {
-    let sum = 0;
-    this.marks.forEach(item => sum += item);
-    return sum / this.marks.length;
-    }
+  }
+  let sum = 0;
+  this.marks.forEach(item => sum += item);
+  return sum / this.marks.length;
   };
 
 Student.prototype.exclude = function (reason) {
   delete this.subject;
   delete this.marks;
-  this.exclude = "плохая учеба";
+  this.excluded = reason;
 }
